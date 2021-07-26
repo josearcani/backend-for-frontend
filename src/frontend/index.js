011d81894a3b44c481cafbcf0b3e3c45d53a9037
josearc <juanjose.arcani2309@gmail.com>
2021-07-26 16:03:29 -0400

    Class 5 - Installing redux-thunk

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { createBrowserHistory } from 'history'; // creamos el historial
 import { Router } from 'react-router'; // importamos o instalamos como depProducción
 
@@ -9,9 +10,9 @@ import reducer from './reducer';
 import App from './routes/App';
 
 const history = createBrowserHistory();
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const preloadedState = window.__PRELOADED_STATE__;
-const store = createStore(reducer, preloadedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(thunk)));
 
 delete window.__PRELOADED_STATE__;
 
