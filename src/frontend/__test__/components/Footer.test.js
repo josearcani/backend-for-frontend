eed8c7a9c4b4d2c04d0e9fa4e8c918f321205994
josearc <juanjose.arcani2309@gmail.com>
2021-08-07 17:33:06 -0400

    Class 15 - Using Snapshots and updating them

--- src/frontend/__test__/components/Footer.test.js
+++ src/frontend/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 
 import Footer from '../../components/Footer';
 
@@ -14,4 +15,10 @@ describe('<Footer />', () => {
   test('Render Has 3 anchors', () => {
     expect(footer.find('a')).toHaveLength(3);
   });
+
+  test('Footer Snapshot', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
+  });
+
 });
